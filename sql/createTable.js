541e881b16ae5d961b02cc600e4ec7707741d323
seanrw93 <seanrw@gmail.com>
2026-03-03 09:34:02 +0100

    hide sql commands

--- sql/createTable.js
+++ sql/createTable.js
@@ -33,16 +33,16 @@ export const createTable = async () => {
             // `
 
 
-            `
-                CREATE TABLE IF NOT EXISTS cart_items(
-                    id SERIAL PRIMARY KEY,
-                    user_id INTEGER NOT NULL,
-                    product_id INTEGER NOT NULL,
-                    quantity INTEGER NOT NULL DEFAULT 1,
-                    FOREIGN KEY (user_id) REFERENCES users(id),
-                    FOREIGN KEY (product_id) REFERENCES products(id) 
-                )
-            `
+            // `
+            //     CREATE TABLE IF NOT EXISTS cart_items(
+            //         id SERIAL PRIMARY KEY,
+            //         user_id INTEGER NOT NULL,
+            //         product_id INTEGER NOT NULL,
+            //         quantity INTEGER NOT NULL DEFAULT 1,
+            //         FOREIGN KEY (user_id) REFERENCES users(id),
+            //         FOREIGN KEY (product_id) REFERENCES products(id) 
+            //     )
+            // `
         );
     } catch (err) {
         console.log("Unable to create table", err);
