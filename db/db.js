ed880739db8c6fdbb1ff7b81935868bd40bda25c
seanrw93 <seanrw@gmail.com>
2026-03-12 22:53:56 +0100

    fix(db): Include SSL in pool config

--- db/db.js
+++ db/db.js
@@ -1,13 +1,3 @@
-// import sqlite3 from "sqlite3";
-// import { open } from "sqlite";
-// import path from "node:path";
-
-// export const getDBConnection = async () => {
-//     return open({
-//         filename: path.join("database.db"),
-//         driver: sqlite3.Database
-//     });
-// }
 
 import pkg from "pg";
 import dotenv from "dotenv";
